b7f9c61ce15c997bb119aef8dfee6307ccacdba3
Minro <1747177608@qq.com>
2019-04-17 20:53:09 +0800

    修改bug

--- www/js/index.js
+++ www/js/index.js
@@ -1,45 +1,25 @@
 document.addEventListener("deviceready", onDeviceReady);
 
-var isReplay = true;    //播放准备状态
-var media = null;   //播放对象
-var btn2Play = false;   //按钮播放状态
-var tid1,tid2;  //进度定时器ID
+var isReplay = true; //播放准备状态
+var media = null; //播放对象
+var btn2Play = false; //按钮播放状态
+var tid1, tid2; //进度定时器ID
+var id; //当前播放的歌曲id
 
 function onDeviceReady() {
     console.log("启动");
-
-    //------添加歌曲列表--------
-    // var md = '<li data-icon="false" id="@liid">\n' +
-    //     '                    <a style="background-color: white;">\n' +
-    //     '                        <div class="bianhao">@musicid</div>\n' +
-    //     '                        <div class="msg">\n' +
-    //     '                            <h1>@songname</h1>\n' +
-    //     '                            <p>2019-4-12</p>\n' +
-    //     '                        </div>\n' +
-    //     '                    </a>\n' +
-    //     '                </li>';
-    // var songs = ['G.E.M.邓紫棋 - 泡沫.mp3', 'G.E.M.邓紫棋 - 龙卷风.mp3', '周笔畅 - 岁月神偷.mp3', '火箭少女101 - 卡路里.mp3',
-    //     '韩红、林俊杰 - 飞云之下.mp3'
-    // ];
-    // for (var i = 0; i < songs.length; i++) {
-    //     var tmp = md;
-    //     tmp = tmp.replace("@liid", "m" + i);
-    //     tmp = tmp.replace("@musicid", i + 1);
-    //     tmp = tmp.replace("@songname", songs[i]);
-
-    //     $('#ullist').append(tmp);
-    // }
 
     //------添加事件--------
     //参数1为事件名，参数2为子元素，参数3为触发该事件的回调函数
     $("#ullist").on("click", "li", function () {
         $("li a").css("background", "white"); //所有的LI恢复背景
         $(this.firstElementChild).css("background", "yellow"); //为当前单击的LI设置背景
-        var id = $(this).prop("id"); //获取被单击的LI的id
+        id = $(this).prop("id"); //获取被单击的LI的id
         id = id.replace("m", "");
         //LI的id格式为：m0 m1 m2 ... ,去掉前缀则为数字了，代表播放的歌曲下标
         console.log("点击事件触发！id为" + id);
         //其他实现
+        stopAudio();
         media = null;
         isReplay = true;
         console.log(songs[id]);
@@ -49,10 +29,23 @@ function onDeviceReady() {
     //-------播放按钮事件-------
     $("#bn").on("click", "div", function () {
         $("#bn").children().css("background-color", "white");
-        $(this).css("background-color", "red");
-        var id = $(this).prop("id");
-        if (id == "btn2") {
+        // $(this).css("background-color", "red");
+        var bid = $(this).prop("id");
+        id = Number(id);
+        if (bid == "btn2") {
             changePlayorPause();
+        } else if (bid == "btn1") {
+            console.log('点击上一首');
+            id = id -1;
+            changeSong(id);
+        } else if (bid == "btn3") {
+            console.log('点击下一首');
+            id = id+1;
+            changeSong(id);
+        } else {
+            console.log('点击停止按钮');
+            stopAudio();
+            afterStop();
         }
     });
 }
@@ -81,7 +74,7 @@ function playAudio(src) {
 
 function replayAudio(src) {
     console.log("开始播放！");
-    src = cordova.file.applicationDirectory+"www/mp3/" + src;
+    src = cordova.file.applicationDirectory + "www/mp3/" + src;
     media = new Media(src,
         function () {
             console.log("play complete...");
@@ -91,6 +84,7 @@ function replayAudio(src) {
         },
         function (code) {
             switch (code) {
+                //-----开始播放时-----
                 case Media.MEDIA_STARTING:
                     afterPlay();
                     console.log("状态：开始播放!");
@@ -101,9 +95,19 @@ function replayAudio(src) {
                         "background-color": "red"
                     });
                     break;
+
+                    //-----进行播放时-----
                 case Media.MEDIA_RUNNING:
+                    //-----旋转图片-----
+                    // var angle = 0;
+                    // for (var i = 0; i < 360; i++) {
+                    //     $("#fimg").css("transform", "rotate(" + angle + "deg)");
+                    //     angle += 5;
+                    // }
                     playPos();
                     break;
+
+                    //-----暂停时-----
                 case Media.MEDIA_PAUSED:
                     console.log("状态：暂停播放！");
                     // $("#btn2").css({
@@ -113,8 +117,12 @@ function replayAudio(src) {
                     // });
                     btn2Play = false;
                     break;
+
+                    //-----停止时-----
                 case Media.MEDIA_STOPPED:
-                    afterStop();
+                    if (isReplay == false) {
+                        afterStop();
+                    }
                     break;
             }
         });
@@ -128,13 +136,19 @@ function stopAudio() {
     //清除进度显示(假如有)，以及重新初始华其他操作，为下一次播放做准备
     if (media != null) {
         media.stop();
+        $("#time").html("完成");
     }
 }
 
 function afterStop() {
-    console.log("播放完成！");
+    console.log("播放完成！初始化了。");
+    // clearInterval(tid2);
+    // console.log("清除定时器tid2");
     isReplay = true;
     media = null; //注意！为了下次可以播放，要初始化
+    btn2Play = true;
+    $('#ullist li a').css("background", "white"); //所有的LI恢复背景
+    $("#a"+id).css("background", "yellow");
 }
 
 function afterPlay() {
@@ -144,9 +158,9 @@ function afterPlay() {
 
 
 function changePlayorPause() {
-    console.log("点击播放/暂停");
+    console.log("点击播放/暂停 按钮");
     if (btn2Play) {
-        console.log("按钮状态："+btn2Play);
+        console.log("按钮状态：" + btn2Play);
         btn2Play = false;
         $("#btn2").css({
             "background-image": "url('" + cordova.file.applicationDirectory + "www/img/播放.png')"
@@ -154,7 +168,7 @@ function changePlayorPause() {
         });
         media.pause();
     } else {
-        console.log("按钮状态："+btn2Play);
+        console.log("按钮状态：" + btn2Play);
         btn2Play = true;
         $("#btn2").css({
             "background-image": "url('" + cordova.file.applicationDirectory + "www/img/暂停.png')"
@@ -165,19 +179,40 @@ function changePlayorPause() {
     // console.log("已暂停！");
 }
 
-function playPos()
-{ 
-   var len=-1;
-   setInterval(function(){
-       len=media.getDuration();
-       if(len>0) {clearInterval(tid1);}
-       len = Math.round(len/60)+":"+Math.round(len)%60;
-    },100);
-  
-    
-   tid2=setInterval(function(){
-     	media.getCurrentPosition(function(pos){
-          $("#time").html("当前播放位置："+pos+"/"+len);//注意修改id
-     	},function(){});
-   },200);
+function playPos() {
+    var len = -1;
+    setInterval(function () {
+        len = media.getDuration();
+        if (len > 0) {
+            clearInterval(tid1);
+        }
+        len = Math.round(len / 60) + ":" + Math.round(len) % 60;
+    }, 100);
+
+    tid2 = setInterval(function () {
+        media.getCurrentPosition(function (pos) {
+            $("#time").html("当前播放位置：" + parseInt(pos / 60) + ":" + (Array(2).join('0') + Math.round(pos) % 60).slice(-2) + "/" + len); //显示播放的进度文字
+        }, function () {});
+    }, 200);
+}
+
+//初始化
+function changeSong(songid) {
+    console.log('点击后，当前的id：' + songid);
+    stopAudio();
+    afterStop();
+    if (songid < 0) {
+        songid = 0;
+        id = 0;
+        console.log(songs[songid]);
+        playAudio(songs[songid]);
+    } else if (songid > songs.length-1) {
+        songid = songs.length-1;
+        id = songid;
+        console.log(songs[songid]);
+        playAudio(songs[songid]);
+    } else {
+        console.log(songs[songid]);
+        playAudio(songs[songid]);
+    }
 }
